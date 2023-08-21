534afe5ed37fe8bad6ce20c71fb0115608f9a725
geqipeng <geqipeng@sina.cn>
2023-08-21 21:11:00 +0800

    feat: flutter布局搭建

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,126 @@ void main() {
   runApp(
     const MaterialApp(
       title: "Flutter layout demo",
-      home:MyApp(),
+      home:MyCustomButton(),
     ),
   );
 }
+
+// 自定义button 组合
+class MyCustomButton extends StatelessWidget{
+  const MyCustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    // 定义titleWidget
+    Widget titleSelection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: const Text(
+                  "Oeschinen Lake Campground",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                "Kandersteg, Switzerland",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              )
+            ],
+          )
+          ),
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text("41"),
+        ],
+      ),
+    );
+
+
+    // button
+    Column _buildButtonColumn(Color color,IconData icon,String label){
+      return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon,color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
+            ),
+          )
+        ],
+      );
+    }
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color,Icons.call, "CALL"),
+        _buildButtonColumn(color,Icons.near_me, "ROUTE"),
+        _buildButtonColumn(color,Icons.share, "SHARE"),
+      ],
+    );
+
+    // text
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: const Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+            'Alps. Situated 1,578 meters above sea level, it is one of the '
+            'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+            'half-hour walk through pastures and pine forest, leads you to the '
+            'lake, which warms to 20 degrees Celsius in the summer. Activities '
+            'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
+      ),
+    );
+
+    // picture
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+        ),
+        body: Column(
+          children: [
+            Image.asset(
+                'images/lake.jpeg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSelection,
+            buttonSection,
+            textSection
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 
 class Product {
   const Product({required this.name});
